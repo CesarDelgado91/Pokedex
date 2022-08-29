265b5d38ab584e8fa1fcba4939a3ced1afe4b268
cesardelgado <tommydr53@gmail.com>
2022-08-28 22:44:04 -0500

    avances en busqueda

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 
 
 let pokemones;
-const modal = document.querySelector("#modal");
+
 
 fetch("https://raw.githubusercontent.com/oicrruf/g15-computer-science/develop/ejercicios/pokedex-registro/json/pokemon.json")
   .then(response => response.json())
   .then((data) => {
     pokemones = data;
-    dibujar();
+    dibujar(pokemones);
 });
 
-let dibujar = () => {
+let dibujar = (pokemonesArr) => {
 
     document.querySelector("#resultados").innerHTML="";
 
-    for(let i in pokemones){
-        console.log(pokemones[i]);
+    for(let i in pokemonesArr){
+        // console.log(pokemones[i]);
 
         let col = document.createElement("div");
         col.classList.add("col","col-lg-3");
@@ -24,7 +24,7 @@ let dibujar = () => {
         card.classList.add("card","mt-5");
 
         let img = document.createElement("img")
-        img.src=pokemones[i].ThumbnailImage;
+        img.src=pokemonesArr[i].ThumbnailImage;
         img.classList.add("card-img-top");
 
         card.append(img);
@@ -34,29 +34,27 @@ let dibujar = () => {
 
         let h5 = document.createElement("h5");
         h5.classList.add("card-title")
-        h5.innerHTML=pokemones[i].name;
+        h5.innerHTML=pokemonesArr[i].name;
         cardBody.append(h5);
 
         let a = document.createElement("a");
         a.setAttribute("href","#")
         a.classList.add("btn","btn-primary");
         a.innerHTML="Informacion";
-        a.setAttribute("data-number",pokemones[i].number);
-        a.dataset.name=pokemones[i].name;
-        a.dataset.type=pokemones[i].type;
+        a.setAttribute("data-number",pokemonesArr[i].number);
+        a.dataset.name=pokemonesArr[i].name;
+        a.dataset.type=pokemonesArr[i].type;
 
         a.addEventListener("click",function(evt){
             evt.preventDefault();
             alert(`${evt.target.dataset.name},  ${evt.target.dataset.number}, ${evt.target.dataset.type} `);
         });
 
-
         cardBody.append(a);
-
         card.append(cardBody)
         col.append(card);
 
         document.querySelector("#resultados").append(col);
-    }
+    } 
 
 }
--- js/filters.js
+++ js/filters.js
@@ -1,15 +1,20 @@
+
+
 let buscar = (evt) => {
-  
-    let name = document.querySelector("#inputBusqueda").value.toLowerCase();
+    evt.preventDefault();
+    let name = document.querySelector("#inputBusqueda").value;
     let filtrados = pokemones.filter((pokemon) => {
-      
       return pokemon.name.toLowerCase().includes(name);
     });
- dibujar(filtrados);
-  };
+    // console.log(filtrados);
+
+   dibujar(filtrados);
+  }
   
+  document.querySelector("#busqueda").addEventListener("submit", buscar); 
   
-  //document.querySelector("#busqueda").addEventListener("submit", buscar);
-document.querySelector("#busqueda button").addEventListener("click", buscar);
+// document.querySelector("#busqueda button").addEventListener("click", buscar);
 // keydown, keyup, keypress
-document.querySelector("#inputBusqueda").addEventListener("keyup", buscar);
+  document.querySelector("#inputBusqueda").addEventListener("keyup", buscar);
+
+  
